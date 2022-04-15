1464532688230837e2c2066e7699e9b3c2927434
superfishal <97133716+superfishal@users.noreply.github.com>
2022-04-15 15:53:45 -0700

    updated database allowances

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: "root",
     // Your MySQL password
-    password: "Rtfc81533!",
+    password: "",
     database: "election",
   },
   console.log("Connected to the election database.")
